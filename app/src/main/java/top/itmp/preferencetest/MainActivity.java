ca4a7dc214883c1965477f3f3a11147f5548bdca
leehz <hz@huaixiaoz.com>
2015-12-14 15:08:12 +0800

    initial commit

---
+++ app/src/main/java/top/itmp/preferencetest/MainActivity.java
@@ -0,0 +1,43 @@
+package top.itmp.preferencetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        menu.add(Menu.NONE, Menu.FIRST+1, 0, "heh").setIcon(R.mipmap.ic_launcher);
+        //return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu:
+                break;
+            case Menu.FIRST + 1:
+                Toast.makeText(this, "heh 被电击", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu1:
+                break;
+            case R.id.menu2:
+                break;
+            case R.id.menu3:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
