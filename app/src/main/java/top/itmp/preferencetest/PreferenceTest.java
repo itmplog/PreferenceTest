ee390ca04665434675b721d75362f7d5faa776b5
leehz <hz@huaixiaoz.com>
2015-12-14 18:10:35 +0800

    fix back button to home

--- app/src/main/java/top/itmp/preferencetest/PreferenceTest.java
+++ app/src/main/java/top/itmp/preferencetest/PreferenceTest.java
@@ -2,6 +2,7 @@ package top.itmp.preferencetest;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
 
 
 import top.itmp.preferencetest.R;
@@ -16,5 +17,11 @@ public class PreferenceTest extends AppCompatPreferenceActivity{//PreferenceActi
       //  AppCompatDelegate.create(this, null).getSupportActionBar().setTitle(getString(R.string.settings));
 
         addPreferencesFromResource(R.xml.preference);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        NavUtils.navigateUpFromSameTask(this);
     }
 }
