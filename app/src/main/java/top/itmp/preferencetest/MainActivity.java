26bdc96ee1209d55c8cb04bd30732b9750ec2148
leehz <hz@huaixiaoz.com>
2015-12-16 13:07:15 +0800

    add swipe view PreferenceFragment

--- app/src/main/java/top/itmp/preferencetest/MainActivity.java
+++ app/src/main/java/top/itmp/preferencetest/MainActivity.java
@@ -1,19 +1,25 @@
 package top.itmp.preferencetest;
 
-import android.app.ActionBar;
-import android.app.Fragment;
+
+
+
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.preference.PreferenceFragment;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static Integer[] preferences = {R.xml.preference, R.xml.preferencefrag, R.xml.preferencefrag};
+    private ViewPager viewPager = null;
+    private FragmentPagerAdapter fragmentPagerAdapter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +30,13 @@ public class MainActivity extends AppCompatActivity {
        // ActionBar actionBar = getActionBar();
        //actionBar.setDisplayShowTitleEnabled(false);
        // final ViewPager viewPager = (ViewPager)findViewById(R.id.container);
-        getFragmentManager().beginTransaction().add(android.R.id.content, new PreferenceFragmentTest.PreferenceFragment0()).commit();
+
+        viewPager = (ViewPager)findViewById(R.id.container);
+        fragmentPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+        viewPager.setAdapter(fragmentPagerAdapter);
+
+
+       // getFragmentManager().beginTransaction().add(android.R.id.content, new PreferenceFragmentTest.PreferenceFragment0()).commit();
 
     }
 
@@ -55,5 +67,63 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class PlaceholderFragment extends PreferenceFragment {
+        private static final String ARG_SECTION_NUMBER = "section_number";
+        private  int i= 0;
+
+        public PlaceholderFragment() {
+        }
+
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            if((i = getArguments().getInt(ARG_SECTION_NUMBER)) != 0) {
+                addPreferencesFromResource(preferences[i-1]);
+            }
+        }
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public PreferenceFragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
     }
 }
